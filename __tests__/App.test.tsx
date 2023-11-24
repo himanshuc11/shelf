2dd51627b4b6607d99cb4523a542f292ddea069e
himanshuc11 <himanshuchhatpar@gmail.com>
2023-11-25 00:05:11 +0530

    test: Added tab bar tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -7,11 +7,24 @@ import React from 'react';
 import App from '../App';
 
 // Note: import explicitly to use the types shiped with jest.
-import {it} from '@jest/globals';
+import {it, expect} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer, {act} from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+it('tab bar focusses correctly', () => {
+  const app = renderer.create(<App />);
+  const chartButton = app.root.findByProps({testID: 'Chart'});
+  const homeButton = app.root.findByProps({testID: 'Home'});
+
+  expect(homeButton.props.accessibilityState.selected).toBe(true);
+  act(() => {
+    chartButton.props.onPress();
+  });
+  expect(chartButton.props.accessibilityState.selected).toBe(true);
+  expect(homeButton.props.accessibilityState.selected).toBe(undefined);
 });
--- screens/Guide.tsx
+++ screens/Guide.tsx
@@ -2,7 +2,6 @@ import {View} from 'react-native';
 import ItenaryItem from '../components/ItenaryItem';
 import type {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
 import type {HomeStackNavigatorParamList} from '../types';
-import useNotificationToken from '../hooks/useNotificationToken';
 
 type GuideScreenNavigationProp = BottomTabScreenProps<
   HomeStackNavigatorParamList,
@@ -10,7 +9,6 @@ type GuideScreenNavigationProp = BottomTabScreenProps<
 >;
 
 function Guide(props: GuideScreenNavigationProp) {
-  useNotificationToken();
   return (
     <View style={{flex: 1, marginTop: 30}}>
       <ItenaryItem />
