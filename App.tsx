e085bbdb70989c03cc6b2d4f860a80a7010fda49
himanshuc11 <himanshuchhatpar@gmail.com>
2023-11-24 23:19:58 +0530

    feat: Added custom tab bar as per UI

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-
+import TabBar from './components/TabBar';
 import {NavigationContainer} from '@react-navigation/native';
 import {HomeStackNavigatorParamList} from './types';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -12,7 +12,7 @@ const Tab = createBottomTabNavigator<HomeStackNavigatorParamList>();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Navigator screenOptions={{headerShown: false}} tabBar={TabBar}>
         <Tab.Screen
           name="Home"
           component={Screen.Home}
@@ -24,9 +24,39 @@ export default function App() {
             ),
           }}
         />
-        <Tab.Screen name="Wallet" component={Screen.Wallet} />
-        <Tab.Screen name="Guide" component={Screen.Guide} />
-        <Tab.Screen name="Chart" component={Screen.Chart} />
+        <Tab.Screen
+          name="Wallet"
+          component={Screen.Wallet}
+          options={{
+            tabBarIcon: props => (
+              <Icons.Wallet
+                fill={props.focused ? COLORS.FOCUS_BLUE : COLORS.ICON_GRAY}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Guide"
+          component={Screen.Guide}
+          options={{
+            tabBarIcon: props => (
+              <Icons.Guide
+                fill={props.focused ? COLORS.FOCUS_BLUE : COLORS.ICON_GRAY}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Chart"
+          component={Screen.Chart}
+          options={{
+            tabBarIcon: props => (
+              <Icons.Chart
+                fill={props.focused ? COLORS.FOCUS_BLUE : COLORS.ICON_GRAY}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
