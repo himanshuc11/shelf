b025242d40c1de91b8fd73195547997b29582f35
himanshuc11 <himanshuchhatpar@gmail.com>
2023-11-26 10:37:22 +0530

    fix: Fixed timeline lines issue because of incorred params passed

--- screens/Guide.tsx
+++ screens/Guide.tsx
@@ -1,16 +1,39 @@
 import {View, FlatList} from 'react-native';
 import {ITEMS} from '../utils/constants';
 import ItenaryItem from '../components/ItenaryItem';
+import GuideHeader from '../components/GuideHeader';
+import {generateDummyData} from '../utils/helper';
+import {useSelector, useDispatch} from 'react-redux';
 import type {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
 import type {HomeStackNavigatorParamList} from '../types';
-import GuideHeader from '../components/GuideHeader';
+import type {GuideTab} from '../types';
+import {useMemo} from 'react';
 
 type GuideScreenNavigationProp = BottomTabScreenProps<
   HomeStackNavigatorParamList,
   'Guide'
 >;
 
+const items = generateDummyData();
+
 function Guide(props: GuideScreenNavigationProp) {
+  const guide = useSelector<{guide: GuideTab}>(
+    state => state.guide,
+  ) as GuideTab;
+
+  const currentItems = useMemo(() => {
+    switch (true) {
+      case guide === 'YESTERDAY':
+        return items.yesterdayItems;
+      case guide === 'TODAY':
+        return items.todayItems;
+      case guide === 'TOMORROW':
+        return items.tommorowItems;
+      default:
+        return items.todayItems;
+    }
+  }, [guide]);
+
   return (
     <View style={{flex: 1}}>
       <GuideHeader />
@@ -18,12 +41,12 @@ function Guide(props: GuideScreenNavigationProp) {
         contentContainerStyle={{
           paddingTop: 32,
         }}
-        data={ITEMS}
+        data={currentItems}
         renderItem={({item, index}) => (
           <ItenaryItem
             {...item}
             key={item.id}
-            nextDate={ITEMS?.[index + 1]?.date}
+            nextDate={currentItems?.[index + 1]?.date}
           />
         )}
       />
