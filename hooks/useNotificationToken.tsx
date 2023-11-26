a8d1f6a5dbfccadf4cc56b1086a3b8952d0b4d88
himanshuc11 <himanshuchhatpar@gmail.com>
2023-11-26 14:58:11 +0530

    chore: Added uri scheme for linking

--- hooks/useNotificationToken.tsx
+++ hooks/useNotificationToken.tsx
@@ -1,18 +1,19 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {
   requestNotificationPermissionAndroid,
   getFirebaseTokenAsync,
 } from '../utils/helper';
 
 function useNotificationToken() {
+  const [token, setToken] = useState(null);
   useEffect(() => {
     const token = async () => {
       await requestNotificationPermissionAndroid();
       const res = await getFirebaseTokenAsync();
-      // console.log(res);
     };
     token();
   }, []);
+  return token;
 }
 
 export default useNotificationToken;
